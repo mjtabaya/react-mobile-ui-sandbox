35673b92f88d3430de0fb096974bc27aa0b12caa
mjtabaya <mjtabaya@gmail.com>
2021-12-26 01:12:56 +0800

    Mushi use

--- src/screens/public/Dashboard/index.tsx
+++ src/screens/public/Dashboard/index.tsx
@@ -1,17 +1,80 @@
 import * as React from 'react';
-import { Text } from 'react-native-paper';
-import { useAuth } from '@hooks/useAuth';
-import * as S from './styled';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Appbar, Button, Text, Card, Title, Paragraph } from 'react-native-paper';
 
 const DashboardScreen = () => {
-  const { user } = useAuth();
-  const { firstName } = user;
-
+  const state = {
+    event: {
+      name: 'Test',
+      date: 'Dec 6, 2022',
+    },
+  };
+  const onSuccess = (params) => {
+    console.log(params);
+  };
+  const navigate = (params) => {
+    console.log(params);
+  };
+  const startScan = () => {
+    console.log(params);
+  };
+  const signOut = () => {
+    console.log('wee');
+  };
+  const setScan = (params) => {
+    console.log(params);
+  };
+  const scan = false;
   return (
-    <S.StyledView>
-      <Text>Hello {firstName}!</Text>
-    </S.StyledView>
+    <View style={styles.container}>
+      <Appbar style={styles.top}>
+        <Appbar.Action icon="subdirectory-arrow-left" onPress={() => navigate(-1)} />
+        <Appbar.Action icon="logout" onPress={signOut} />
+      </Appbar>
+      <View>
+        <Card>
+          <Card.Content>
+            <Title>{state.event.name}</Title>
+            <Paragraph>{state.event.date}</Paragraph>
+            {!scan && (
+              <Button mode="contained" style={styles.input} onPress={startScan}>
+                Start Scan
+              </Button>
+            )}
+            {scan && (
+              <Button mode="contained" style={styles.input} onPress={() => setScan(false)}>
+                Stop Scan
+              </Button>
+            )}
+          </Card.Content>
+        </Card>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  input: {
+    height: 40,
+    margin: 40,
+    justifyContent: 'center',
+  },
+  top: {
+    left: 0,
+    right: 0,
+    top: 0,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  right: {
+    right: 0,
+  },
+  sectionContainer: {
+    marginTop: 32,
+  },
+});
 
 export default DashboardScreen;
